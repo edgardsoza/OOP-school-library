dfd3a44a96a878232a8e9d878c907778622f08ce
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-05-24 12:04:39 -0600

    Fix classroom

--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ require_relative './classroom'
 class Student < Person
   def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
-    self.classroom = classroom
+    @classroom = classroom 
   end
 
   def play_hooky
