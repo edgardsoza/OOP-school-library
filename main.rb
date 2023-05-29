b81140183cb3c865b7a9d53aad47d4678006cda9
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-05-29 14:02:29 -0600

    Fix issue with cycles

--- main.rb
+++ main.rb
@@ -4,13 +4,13 @@ class MainPage
   def initialize(app)
     @app = app
     @options = {
-      '1' => 'List all books',
-      '2' => 'List all people',
-      '3' => 'Create a person',
-      '4' => 'Create a book',
-      '5' => 'Create a rental',
-      '6' => 'List all rentals for a given person id',
-      '7' => 'Exit'
+      '1' => [:list_all_books, 'List all books'],
+      '2' => [:list_all_people, 'List all people'],
+      '3' => [:create_person, 'Create a person'],
+      '4' => [:create_book, 'Create a book'],
+      '5' => [:create_rental, 'Create a rental'],
+      '6' => [:rental_list, 'List all rentals for a given person id'],
+      '7' => [:exit_app, 'Exit']
     }
   end
 
@@ -18,29 +18,45 @@ class MainPage
     puts 'Welcome to School Library Home Page!'
     puts 'Please enter a number: '
 
-    @options.each { |index, string| puts "#{index} - #{string}" }
+    @options.each { |index, (_method_name, string)| puts "#{index} - #{string}" }
   end
 
   def handle_option(option)
-    case option
-    when '1'
-      @app.list_all_books
-    when '2'
-      @app.list_all_people
-    when '3'
-      @app.create_person
-    when '4'
-      @app.create_book
-    when '5'
-      @app.create_rental
-    when '6'
-      @app.rental_list
-    when '7'
-      puts 'I hope you enjoyed our app'
-      exit
+    action = @options[option]
+    if action
+      send(action[0])
     else
       puts 'The selected option does not exist'
     end
+  end
+
+  def list_all_books
+    @app.list_all_books
+  end
+
+  def list_all_people
+    @app.list_all_people
+  end
+
+  def create_person
+    @app.create_person
+  end
+
+  def create_book
+    @app.create_book
+  end
+
+  def create_rental
+    @app.create_rental
+  end
+
+  def rental_list
+    @app.rental_list
+  end
+
+  def exit_app
+    puts 'I hope you enjoyed our app'
+    exit
   end
 
   def run
