28345db153f13b75df93c40406b255080c2fdfbc
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-05-30 11:57:24 -0600

    Fix linters

--- main.rb
+++ main.rb
@@ -10,55 +10,54 @@ def menu
   puts '5 - Create a rental'
   puts '6 - List all rentals for a given person id'
   puts '7 - Exit'
-  option = gets.chomp.to_i
-  option
+  gets.chomp.to_i
 end
 
-  def handle_option(option)
-    app = App.new
-    case option
-      when 1
-        app.list_all_books
-      when 2
-        app.list_all_people
-      when 3
-        app.create_person
-      when 4
-        app.create_book
-      when 5
-        app.create_rental
-      when 6
-        app.rental_list
-      when 7
-        exit_app
-      else
-        invalid_option
-      end
+def handle_option(option)
+  app = App.new
+  case option
+  when 1
+    app.list_all_books
+  when 2
+    app.list_all_people
+  when 3
+    app.create_person
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    app.rental_list
+  when 7
+    exit_app
+  else
+    invalid_option
   end
+end
 
-  def invalid_option
-    puts 'Invalid option. Please try again.'
-    option = gets.chomp.to_i
+def invalid_option
+  puts 'Invalid option. Please try again.'
+  option = gets.chomp.to_i
+  handle_option(option)
+end
+
+def display_options
+  puts 'Welcome to School Library Home Page!'
+  puts 'Please enter a number: '
+
+  options.each { |index, string| puts "#{index} - #{string}" }
+end
+
+def exit_app
+  puts 'I hope you enjoyed our app'
+  exit
+end
+
+def main
+  loop do
+    option = menu
     handle_option(option)
   end
-
-  def display_options
-    puts 'Welcome to School Library Home Page!'
-    puts 'Please enter a number: '
-
-    options.each { |index, string| puts "#{index} - #{string}" }
-  end
-
-  def exit_app
-    puts 'I hope you enjoyed our app'
-    exit
-  end
-
-  def main
-    loop do
-      option = menu
-      handle_option(option)
-    end
-  end
+end
 
 main
